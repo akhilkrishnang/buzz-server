e787d7bfa6fdbfa542de936d3a0184d87355a869
akhilkrishnang <akhilrohini@gmail.com>
2020-01-04 13:28:07 +0530

    ENV vars name change

--- queries.js
+++ queries.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
     database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DB_PORT,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
     sslmode : 'require',
     ssl : 'true',
     sslfactory:'org.postgresql.ssl.NonValidatingFactory'
